deb4d56a0adc7bc52f741d6169c34ed665a4150e
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-12-15 15:13:47 +0100

    ddsim steering improved

--- steering.py
+++ steering.py
@@ -4,16 +4,24 @@ from DDSim.DD4hepSimulation import DD4hepSimulation
 SIM = DD4hepSimulation()
 
 ## if there is a user provided SDAction which needs additional parameters these can be passed as a dictionary
-SIM.action.mapActions["MY_FIRST_CUBE"] = ( "NeutronSDAction", {"NeutronEnergyThreshold": "1*GeV"} )
+SIM.action.mapActions["MY_FIRST_CUBE"] = "mySDaction"
 
-SIM.filter.tracker = "edep0"
+SIM.filter.tracker = ""
+SIM.filter.calo    = ""
+SIM.filter.filters = {}
 
 # Particle gun settings
 SIM.numberOfEvents = 10
 SIM.enableGun = True
-SIM.gun.energy = "1*GeV"
-SIM.gun.particle = "neutron"
+SIM.gun.energy = "100*GeV"
+SIM.gun.particle = "proton"
 SIM.gun.direction = "0 0 -1"
 SIM.gun.multiplicity = 1
 SIM.gun.position = "0 0 1*cm"
 
+SIM.random.enableEventSeed = True
+SIM.random.file = None
+SIM.random.luxury = 1
+SIM.random.replace_gRandom = True
+SIM.random.seed = 1
+SIM.random.type = None
